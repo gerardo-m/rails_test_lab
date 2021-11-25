61d46143e6e1c0887f6632cd7d06fb99616f638c
Gerardo Miranda <https://GerardoMiranda@git.assembla.com>
2021-11-24 22:01:40 -0400

    Added methods and benchmarking

--- lib/generating_data.rb
+++ lib/generating_data.rb
@@ -30,13 +30,30 @@ module GeneratingData
         values = []
         columns = ['dummy_text1', 'dummy_text2', 'dummy_text3']
         (1..size).each do |i|
-            values << [Faker::Lorem.word,Faker::Lorem.word,Faker::Lorem.word]
+            record = generate_record
+            values << [record.dummy_text1, record.dummy_text2, record.dummy_text3]
         end
         DummyModel.import(columns, values, validate: false)
     end
 
     def self.generate_record
         DummyModel.new(dummy_text1: Faker::Lorem.word, dummy_text2: Faker::Lorem.word, dummy_text3: Faker::Lorem.word)
+    end
+
+    def self.execute_benchmarking(size)
+        r1 = Benchmark.measure{with_active_record_create(size)}
+        r2 = Benchmark.measure{with_active_record_import(size)}
+        r3 = Benchmark.measure{with_active_record_import_without_validations(size)}
+        r4 = Benchmark.measure{with_active_record_import_columns_without_validations(size)}
+        clean
+        puts "With ActiveRecord create"
+        puts r1
+        puts "With ActiveRecord import"
+        puts r2
+        puts "With ActiveRecord import without validations"
+        puts r3
+        puts "With ActiveRecord using columns without validations"
+        puts r4
     end
 
     def self.clean
